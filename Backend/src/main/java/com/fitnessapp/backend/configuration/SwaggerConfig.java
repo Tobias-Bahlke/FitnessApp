f5dceb778091e944399137ff97914294375cf43c
Tobias-Bahlke <tobias.bahlke@web.de>
2024-10-24 01:29:45 +0200

    Started to create global styles for the frontend and started to initialize the backend

---
+++ Backend/src/main/java/com/fitnessapp/backend/configuration/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.fitnessapp.backend.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("FitnessApp API")
+                        .version("1.0")
+                        .description("Erste offizielle Version der API der FitnessApp")
+                        .contact(new Contact().name("Tobias Bahlke").email("tobias.bahlke@web.de"))
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization"))
+                        .addResponses("401", new ApiResponse().description("Unauthorized"))
+                        .addResponses("403", new ApiResponse().description("Forbidden"))
+                )
+                .addTagsItem(new Tag().name("auth").description("Endpunkte für die Authentifizierung"))
+
+    }
+}
---
+++ Backend/src/main/java/com/fitnessapp/backend/configuration/security/SimpleCorsFilter.java
@@ -0,0 +1,50 @@
+package com.fitnessapp.backend.configuration.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+    @Value("${app.client.url}")
+    private String clientAppUrl;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        String origin = request.getHeader("Origin");
+
+        if (origin != null && origin.equals(clientAppUrl)) {
+            response.setHeader("Access-Control-Allow-Origin", clientAppUrl);
+        }
+
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
